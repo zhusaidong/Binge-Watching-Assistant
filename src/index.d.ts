661ec2cf492faff16c1fd7651000a3606d5c48f7
zhusaidong <635925926@qq.com>
2024-10-17 17:19:39 +0800

    feature: 添加对象的类型定义文件

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,6 @@
+/**
+ * 书签节点
+ */
 export interface BookmarkTreeNode {
     /**
      * 是文件夹的标识
@@ -26,17 +29,41 @@ export interface BookmarkTreeNode {
     /**
      * 子节点
      */
-    children: BookmarkTreeNode[];
+    children: Index[];
 }
 
+/**
+ * 新特性提醒
+ */
 export interface FeatureTips {
+    /**
+     * 设置
+     */
     settings: {};
+    /**
+     * 新特性组
+     */
     features: FeatureTip[]
 }
 
+/**
+ * 新特性属性
+ */
 export interface FeatureTip {
+    /**
+     * 版本号
+     */
     version: string;
+    /**
+     * 新特性key
+     */
     key: string;
+    /**
+     * 新特性消息
+     */
     message: string;
+    /**
+     * 消息是否是html模式
+     */
     isHtml: boolean;
 }
